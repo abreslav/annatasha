74ed09fed0cc7a985f4b1478669793086b2b36c0
abreslav <abreslav@068e5ed6-d4e1-11dd-a9c2-f1de684657c9>
2009-01-26 12:09:21 +0000

    Example added, useless stuff deleted

--- trunk/annotations/src/com/google/code/annatasha/annotations/Method.java
+++ trunk/annotations/src/com/google/code/annatasha/annotations/Method.java
@@ -22,48 +22,4 @@ public interface Method {
 		Class<?>[] value();
 	}
 
-	/**
-	 * Specifies that the method always returns the same value, called at the
-	 * same object with.
-	 * 
-	 * Must validate that overriding method also specifies this attribute if
-	 * overriden method does.
-	 * 
-	 * @author Ivan Egorov
-	 */
-	@Retention(RetentionPolicy.RUNTIME)
-	@Target( { ElementType.METHOD, ElementType.CONSTRUCTOR })
-	public @interface TimeImmutable {
-	}
-
-//	/**
-//	 * The result of method is marked with specified class marker. May be used
-//	 * in further constraints.
-//	 * 
-//	 * @author Ivan Egorov
-//	 * 
-//	 */
-//	@Retention(RetentionPolicy.RUNTIME)
-//	@Target( { ElementType.METHOD, ElementType.CONSTRUCTOR })
-//	@Inherited
-//	public @interface MarkedResult {
-//		Class<?> value();
-//
-//		/**
-//		 * <li>If <code>scope</code> is <code>Scope.Global</code> then
-//		 * marker may be passed without limitations.
-//		 * <li>If <code>scope</code> is <code>Scope.Enclosing</code> then
-//		 * marker is verified to live no longer than the instance on which the
-//		 * method is called. This constraint is checked via following control
-//		 * algorithm:
-//		 * <li>1. the method must be called on instance (I), referenced through
-//		 * local stack (or through the chain of final fields);
-//		 * <li>2. the marker (M) may be bound only to the values on local stack
-//		 * at the depth at least equal to the call, that returned it
-//		 * 
-//		 * @return
-//		 */
-//		Scope scope() default Scope.Global;
-//	}
-
 }
--- trunk/annotations/src/com/google/code/annatasha/annotations/Scope.java
+++
@@ -1,6 +0,0 @@
-package com.google.code.annatasha.annotations;
-
-public enum Scope {
-	Global, 
-	Enclosing
-}
