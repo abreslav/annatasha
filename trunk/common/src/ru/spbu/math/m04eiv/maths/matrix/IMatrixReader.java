b1bc812ea21ac04435e00c82207fb2b88f7fbb8c
egorich.3.04 <egorich.3.04@068e5ed6-d4e1-11dd-a9c2-f1de684657c9>
2009-03-26 18:01:00 +0000

    Changes in example

--- trunk/common/src/ru/spbu/math/m04eiv/maths/matrix/IMatrixReader.java
+++ trunk/common/src/ru/spbu/math/m04eiv/maths/matrix/IMatrixReader.java
@@ -2,8 +2,7 @@ package ru.spbu.math.m04eiv.maths.matrix;
 
 import com.google.code.annatasha.annotations.ThreadMarker;
 
-interface NonThreadMarker {}
 @ThreadMarker
-public interface IMatrixReader extends NonThreadMarker {
+public interface IMatrixReader {
 
 }
--- trunk/common/src/ru/spbu/math/m04eiv/maths/matrix/IMatrixWriter.java
+++ trunk/common/src/ru/spbu/math/m04eiv/maths/matrix/IMatrixWriter.java
@@ -3,6 +3,6 @@ package ru.spbu.math.m04eiv.maths.matrix;
 import com.google.code.annatasha.annotations.ThreadMarker;
 
 @ThreadMarker
-public interface IMatrixWriter {
+public interface IMatrixWriter extends IMatrixReader {
 
 }
