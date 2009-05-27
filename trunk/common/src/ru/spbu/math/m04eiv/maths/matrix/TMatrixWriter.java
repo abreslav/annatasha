1c56e983eed13343c15ad2baf4afe849dd66112e
egorich.3.04 <egorich.3.04@068e5ed6-d4e1-11dd-a9c2-f1de684657c9>
2009-05-27 12:37:19 +0000

    Copyright info embedded

--- trunk/common/src/ru/spbu/math/m04eiv/maths/matrix/TMatrixWriter.java
+++ trunk/common/src/ru/spbu/math/m04eiv/maths/matrix/TMatrixWriter.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 Ivan Egorov <egorich.3.04@gmail.com>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Ivan Egorov <egorich.3.04@gmail.com>
+ *******************************************************************************/
+
 package ru.spbu.math.m04eiv.maths.matrix;
 
 import com.google.code.annatasha.annotations.ThreadMarker;
